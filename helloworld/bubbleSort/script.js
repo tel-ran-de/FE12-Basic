2e56f38edaa2a426026e18e11cdbf4156b6e01a8
Stingrayoliv <64040033+Stingrayoliv@users.noreply.github.com>
2020-10-11 19:57:10 +0200

    added the function to bubble sort

--- helloworld/bubbleSort/script.js
+++ helloworld/bubbleSort/script.js
@@ -1,19 +1,22 @@
 let numbers = [];
 for (let i = 0; i < 5; i++) {
-    numbers[i] = +prompt('Please type some number');
+    numbers[i] = +prompt(`Please type some number ${i + 1}`);
 }
 console.log(`unsorted array: ${numbers}`);
+bubbleSort(numbers);
+console.log(`sorted array: ` + numbers);
 
-unsorted = true;
-while (unsorted) {
-    unsorted = false;
-    for (let i = 0; i < numbers.length; i++) {
-        if (numbers[i] > numbers[i + 1]) {
-            let temp = numbers[i];
-            numbers[i] = numbers[i + 1];
-            numbers[i + 1] = temp;
-            unsorted = true;
+function bubbleSort(numbers) {
+    unsorted = true;
+    while (unsorted) {
+        unsorted = false;
+        for (let i = 0; i < numbers.length; i++) {
+            if (numbers[i] > numbers[i + 1]) {
+                let temp = numbers[i];
+                numbers[i] = numbers[i + 1];
+                numbers[i + 1] = temp;
+                unsorted = true;
+            }
         }
     }
 }
-console.log(`sorted array: ${numbers}`);
