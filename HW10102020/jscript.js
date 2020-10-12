2eb950295537e420756fd5def93d4ee42f9d09a9
OlegSolovyev <olegius.solo@gmail.com>
2020-10-12 11:56:37 +0200

    bubble sort as function

--- HW10102020/jscript.js
+++ HW10102020/jscript.js
@@ -5,17 +5,20 @@ while (i<length){
     arr.push(prompt("your number: "));
     i++;
 }
-
-for (let i = 0; i < length; i++) {
-    for (let j = 0; j < length; j++) {
-        if (arr[j] > arr[j + 1]) {
-            let tmp = arr[j];
-            arr[j] = arr[j + 1];
-            arr[j + 1] = tmp;
-        }
-    }
-}
+arr=bubbleSort(arr);
 
 for (let i=0;i<length;i++) {
     console.log(arr[i]);
+}
+function bubbleSort(arr){
+    for (let i = 0; i < length; i++) {
+        for (let j = 0; j < length; j++) {
+            if (arr[j] > arr[j + 1]) {
+                let tmp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = tmp;
+            }
+        }
+    }
+    return arr;
 }
