18c30015dae2cc5834cbee8baae346f338d3811c
OlegSolovyev <olegius.solo@gmail.com>
2020-10-12 11:34:55 +0200

    bubble sort

---
+++ HW10102020/jscript.js
@@ -0,0 +1,21 @@
+const length=prompt("How many numbers?");
+let i=0;
+let arr=[];
+while (i<length){
+    arr.push(prompt("your number: "));
+    i++;
+}
+
+for (let i = 0; i < length; i++) {
+    for (let j = 0; j < length; j++) {
+        if (arr[j] > arr[j + 1]) {
+            let tmp = arr[j];
+            arr[j] = arr[j + 1];
+            arr[j + 1] = tmp;
+        }
+    }
+}
+
+for (let i=0;i<length;i++) {
+    console.log(arr[i]);
+}
