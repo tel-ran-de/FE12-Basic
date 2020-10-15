1c6a63b6908cb00317301796f9075645c082ff1f
Ievgenij_Pavlenko <evgenij.pavlenko@gmail.com>
2020-10-15 21:37:53 +0200

    write function returning the object containing the frequency of use words

---
+++ frequency_dictionary/script.js
@@ -0,0 +1,33 @@
+let text = "Отец мой Андрей Петрович Гринев в молодости своей служил при графе Минихе и вышел в отставку премьер-майором в 17.. году. С тех пор жил он в своей Симбирской деревне, где и женился на девице Авдотье Васильевне Ю., дочери бедного тамошнего дворянина. Нас было девять человек детей. Все мои братья и сестры умерли во младенчестве."
+
+let frequency = {
+}
+
+let data = text.split(/[ ,.]+/);
+console.log(data);
+
+//add word in Object
+function frequencyFunction() {
+    for (const word of data) {
+        if (frequency[word]) {
+            frequency[word] += 1;
+        } else
+            frequency[word] = 1;
+    }
+}
+
+frequencyFunction();
+
+// copy Object
+let byFrequency = Object.assign(frequency);
+
+//сортирует, только выводит само слово, без количества вхождений
+let sorted = Object.keys(byFrequency).sort(function (a, b) {
+    return byFrequency[b]-byFrequency[a];
+});
+
+//original Object with word + frequency
+    console.log(frequency);
+
+//sorted Object (by frequency)
+    console.log(sorted);
