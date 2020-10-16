d4feae0c0627985acf33b29e6b4673441952fc0d
pointMS <m.sekacheva@gmail.com>
2020-10-16 14:59:20 +0200

    HW 2020-10-12 #34

---
+++ wordsInText/script.js
@@ -0,0 +1,21 @@
+const text = "При вызове функции результат её вызова на предыдущем элементе массива передаётся как первый аргумент. Звучит сложновато, но всё становится проще, если думать о первом аргументе как «аккумулирующем» результат предыдущих вызовов функции"
+console.log(text);
+
+let array = text.split(" ");
+console.log(array);
+
+let result = [];
+calculateWords(array);
+console.log(result);
+
+function calculateWords(array) {
+    for (const word of array) {
+        if (word !== "") {
+            if (result[word]) {
+                result[word] = result[word] + 1;
+            } else {
+                result[word] = 1;
+            }
+        }
+    }
+}
