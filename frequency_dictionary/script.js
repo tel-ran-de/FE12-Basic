1758d1f0bc7677424875e95c17bb1002f2d73ae6
isalexlog <is.alex.loginov@gmail.com>
2020-10-23 21:56:06 +0200

    HW 2020-10-19 (top 3, bottom 3, show all)

--- frequency_dictionary/script.js
+++ frequency_dictionary/script.js
@@ -1,4 +1,15 @@
-function frequencyDictionary(str) {
+let frequencyDictionary;
+
+const comparators = {
+    'bottomToTop': function(a, b) {
+        return a - b
+    },
+    'topToBottom': function(a, b) {
+        return b - a
+    }
+};
+
+function generateFrequencyDictionary(str) {
     const dictionary = {};
 
     const wordsArray = wordsStringToArray(str);
@@ -12,7 +23,35 @@ function frequencyDictionary(str) {
             dictionary[wordsArray[i]] = 1;
         }
     }
-    return dictionary;
+
+    const dictionaryArray = [];
+    for (const word in dictionary) {
+        dictionaryArray.push(
+            {
+                word: word,
+                counter: dictionary[word]
+            }
+        );
+    }
+
+    return dictionaryArray;
+}
+
+function bubbleSort(inArray, comparator) {
+    const arr = [...inArray];
+    let unsorted = true;
+    while (unsorted) {
+        unsorted = false;
+        for (let i = 0; i < arr.length - 1; i++) {
+            if (comparator(arr[i].counter, arr[i + 1].counter) > 0) {
+                let temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+                unsorted = true;
+            }
+        }
+    }
+    return arr;
 }
 
 function wordsStringToArray(str) {
@@ -22,7 +61,7 @@ function wordsStringToArray(str) {
         });
 }
 
-function onButtonClick() {
+function analyseAndInsertIntoTable() {
     const text = document.getElementById('textForAnalysis').value;
     if (text === '') {
         document.querySelector('.input-group div.invalid-feedback.empty').style.display = 'block';
@@ -32,7 +71,8 @@ function onButtonClick() {
         document.querySelector('.input-group div.invalid-feedback.minvalue').style.display = 'block';
         return;
     }
-    insertIntoTable(frequencyDictionary(text));
+    frequencyDictionary = generateFrequencyDictionary(text);
+    insertIntoTable(frequencyDictionary);
 }
 
 function handleOnFocus() {
@@ -41,12 +81,33 @@ function handleOnFocus() {
     }
 }
 
-function insertIntoTable(wordsObject) {
+function insertIntoTable(dictionaryArray) {
     const element = document.querySelector('table.table tbody');
     let toBeInserted = '';
     let i = 1;
-    for (const word in wordsObject) {
-        toBeInserted += `<tr><th scope="row">${i++}</th><td>${word}</td><td>${wordsObject[word]}</td></tr>`;
+    for (const word of dictionaryArray) {
+        toBeInserted += `<tr><th scope="row">${i++}</th><td>${word.word}</td><td>${word.counter}</td></tr>`;
     }
     element.innerHTML = toBeInserted;
+}
+
+function show(startNumber, endNumber, sorted) {
+    if (frequencyDictionary) {
+        if (sorted)
+            insertIntoTable(bubbleSort(frequencyDictionary, comparators[sorted]).slice(startNumber, endNumber));
+        else
+            insertIntoTable(frequencyDictionary.slice(startNumber, endNumber));
+    }
+}
+
+function showTop3() {
+    show(0, 3, 'topToBottom');
+}
+
+function showBottom3() {
+    show(0, 3, 'bottomToTop');
+}
+
+function showAll() {
+    show();
 }
