2723f194b722affc3c285bd0c57233f0757d522c
Ovsiannikov78 <seri_tanya@mail.ru>
2020-10-26 21:47:53 +0100

    Text2Hex - update

--- text2hex/script.js
+++ text2hex/script.js
@@ -1,8 +1,11 @@
 function toHex() {
     const str = document.getElementById('text').value;
     let hex = '';
-    for (let i=0; i < str.length; i++) {
-        hex += str.charCodeAt(i).toString(32) + ' ';
+    for (let i = 0; i < str.length; i++) {
+        hex += str.charCodeAt(i).toString(16) + ' ';
+        if(i > 32){
+            hex += '<br>';
+        }
     }
     document.getElementById('result').innerHTML = hex;
     return hex;
