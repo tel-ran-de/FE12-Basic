0079a84ba93e3b4fac757ed3179ee59b5b25cadf
Stingrayoliv <64040033+Stingrayoliv@users.noreply.github.com>
2020-10-26 15:13:10 +0100

    added pagination

--- frequency_dictionary/script.js
+++ frequency_dictionary/script.js
@@ -1,20 +1,17 @@
 let frequencyDictionary;
 
 const comparators = {
-    'bottomToTop': function(a, b) {
+    'bottomToTop': function (a, b) {
         return a - b
     },
-    'topToBottom': function(a, b) {
+    'topToBottom': function (a, b) {
         return b - a
     }
 };
 
 function generateFrequencyDictionary(str) {
     const dictionary = {};
-
     const wordsArray = wordsStringToArray(str);
-
-    console.log(wordsArray);
 
     for (let i = 0; i < wordsArray.length; i++) {
         if (dictionary[wordsArray[i]]) {
@@ -25,11 +22,13 @@ function generateFrequencyDictionary(str) {
     }
 
     const dictionaryArray = [];
-    for (const word in dictionary) {
+
+    for (let i = 1; i < wordsArray.length; i++) {
         dictionaryArray.push(
             {
-                word: word,
-                counter: dictionary[word]
+                id: i,
+                word: wordsArray[i],
+                counter: dictionary[wordsArray[i]]
             }
         );
     }
@@ -56,7 +55,7 @@ function bubbleSort(inArray, comparator) {
 
 function wordsStringToArray(str) {
     return str.split(/[\s.,?:!;\-"]+/)
-        .filter(function(el) {
+        .filter(function (el) {
             return el.length !== 0
         });
 }
@@ -67,12 +66,11 @@ function analyseAndInsertIntoTable() {
         document.querySelector('.input-group div.invalid-feedback.empty').style.display = 'block';
         return;
     }
-    if (wordsStringToArray(text).length < 2 ) {
+    if (wordsStringToArray(text).length < 2) {
         document.querySelector('.input-group div.invalid-feedback.minvalue').style.display = 'block';
         return;
     }
-    frequencyDictionary = generateFrequencyDictionary(text);
-    insertIntoTable(frequencyDictionary);
+    loadList();
 }
 
 function handleOnFocus() {
@@ -84,9 +82,9 @@ function handleOnFocus() {
 function insertIntoTable(dictionaryArray) {
     const element = document.querySelector('table.table tbody');
     let toBeInserted = '';
-    let i = 1;
-    for (const word of dictionaryArray) {
-        toBeInserted += `<tr><th scope="row">${i++}</th><td>${word.word}</td><td>${word.counter}</td></tr>`;
+
+    for (const row of dictionaryArray) {
+        toBeInserted += `<tr><th scope="row">${row.id}</th><td>${row.word}</td><td>${row.counter}</td></tr>`;
     }
     element.innerHTML = toBeInserted;
 }
@@ -108,6 +106,50 @@ function showBottom3() {
     show(0, 3, 'bottomToTop');
 }
 
-function showAll() {
-    show();
+// global JavaScript variables
+let currentPage = 1;
+let numberPerPage = 5;
+
+function getNumberOfPages() {
+    const text = document.getElementById('textForAnalysis').value;
+    frequencyDictionary = generateFrequencyDictionary(text);
+    return Math.ceil(frequencyDictionary.length / numberPerPage);
+}
+
+function nextPage() {
+    currentPage += 1;
+    loadList();
+}
+
+function previousPage() {
+    currentPage -= 1;
+    loadList();
+}
+
+function firstPage() {
+    currentPage = 1;
+    loadList();
+}
+
+function lastPage() {
+    currentPage = getNumberOfPages();
+    loadList();
+}
+
+function loadList() {
+    let begin = ((currentPage - 1) * numberPerPage);
+    let end = begin + numberPerPage;
+
+    check(); // проверка статусов pagination buttons
+    show(begin, end);
+}
+
+window.onload = loadList;
+
+//Обновление статусов кнопок
+function check() {
+    document.getElementById("next").disabled = currentPage == getNumberOfPages() ? true : false;
+    document.getElementById("previous").disabled = currentPage == 1 ? true : false;
+    document.getElementById("first").disabled = currentPage == 1 ? true : false;
+    document.getElementById("last").disabled = currentPage == getNumberOfPages() ? true : false;
 }
