268e1c19907287f2df8c718df844914138cc056a
Ievgenij_Pavlenko <evgenij.pavlenko@gmail.com>
2020-10-26 20:52:10 +0100

    convert text to HEX.2 - correct with <br>

--- text2hex/script.js
+++ text2hex/script.js
@@ -1,16 +1,17 @@
 let textHEX;
+let lenghtHEXString = 32;
 
-function convertTextToHEX(){
+function convertTextToHEX() {
 
-   const text = document.querySelector('textarea').value;
+    const text = document.querySelector('textarea').value;
+
     for (let i = 0; i < text.length; i++) {
         let a = text.charCodeAt(i);
         let b = a.toString(16);
-        if (i%32===0){
-            console.log("i%32===0");
-            textHEX +='\n';
-        } textHEX += b + " ";
+        if (i % lenghtHEXString === 0) {
+            textHEX += '<br>';
+        }
+        textHEX += b + " ";
     }
     document.querySelector('div.hex').innerHTML = textHEX;
-
 }
