a9675290e458dea9027c935b8ec95e88f60bce6b
mariajanzen <maria_yantsen@yahoo.com>
2020-11-13 20:21:41 +0100

    added actual renewal to driving a car

--- js-oop/car.js
+++ js-oop/car.js
@@ -8,16 +8,24 @@ function Car(model, kmsPerLiter) {
 Car.prototype.fill = function (liters) {
     this.tank += liters;
     console.log('There are ' + this.tank + ' liters in the tank.')
-    return;
 }
 
 Car.prototype.drive = function (distance) {
-    if(this.tank>0) {
+    if(this.tank > 0) {
+        if ((this.tank - distance / this.kmsPerLiter) >= 0) {
+            this.odometr += distance;
+            this.tank -= distance / this.kmsPerLiter;
+            console.log('There car ' + this.model + ' is driving.')
+            console.log('There are ' + this.tank + ' liters in the tank.')
+            return;
+        }
+        distance = this.tank/this.kmsPerLiter;
         this.odometr += distance;
-        this.tank -= distance/this.kmsPerLiter;
-        console.log('There car ' + this.model + ' is driving.')
-        console.log('There are ' + this.tank + ' liters in the tank.')
+        this.tank -= distance / this.kmsPerLiter;
+        console.log('There car ' + this.model + ' can drive only ' + distance + 'km.')
+        console.log('There tank is leer. Please, fill the tank with a fuel.')
         return;
+
     }
     console.log('Please, fill the tank with a fuel.')
 
