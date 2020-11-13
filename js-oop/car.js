00e5c8c53c032ec00da1a4475be09040972177e5
Danil-Makogon <danil_makogon@mail.ru>
2020-11-13 20:28:15 +0100

    HW 2020-11-09 car.js

---
+++ js-oop/car.js
@@ -0,0 +1,21 @@
+function Car(model, kmsPerLiter){
+    this.model=model;
+    this.kmsPerLiter=kmsPerLiter;
+}
+
+Car.prototype.tank=0;
+Car.prototype.odometer=0;
+
+Car.prototype.fill=function (liters) {
+    this.tank += liters;
+}
+
+Car.prototype.drive=function (distance){
+    if(distance<=this.tank * this.kmsPerLiter){
+        this.odometer+= distance;
+        this.tank-= distance / this.kmsPerLiter;
+        return;
+        this.odometer+= this.tank * this.kmsPerLiter;
+    }
+}
+
