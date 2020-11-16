c35ec91daf35ba64db351d986e9468cdf09ce4e9
Ovsiannikov78 <seri_tanya@mail.ru>
2020-11-16 10:29:48 +0100

    HW 2020-11-09 - task Car - code cleaning

--- Task-Car/car.js
+++ Task-Car/car.js
@@ -14,16 +14,18 @@ Car.prototype.fill = function (liters) {
 Car.prototype.drive = function (distance) {
     const distanceТoЕmptyТank = this.tank * (100 / this.fuelConsumption);
 
-    if (this.tank > 0 && distance < distanceТoЕmptyТank) {
+    if (this.tank > 0 && this.odometer <= distanceТoЕmptyТank) {
         this.tank -= (distance * this.fuelConsumption) / 100;
         this.odometer += distance;
         console.log(this.model + ' drove ' + this.odometer + ' km and has ' + this.tank + ' liters of fuel in the tank.');
         return;
 
     }
-    this.odometer = (this.odometer + (this.tank * (100 / this.fuelConsumption)));
+
+    let odometerByEmptyTank = this.odometer + (this.tank * (100 / this.fuelConsumption));
     this.tank = 0;
-    return 'I ran out of fuel at ' + this.odometer + ' km!"';
+    this.odometer = 0;
+    return 'I ran out of fuel at ' + odometerByEmptyTank + ' km!"';
 }
 
 const car1 = new Car('Mercedes', 10);
