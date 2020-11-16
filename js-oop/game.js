5e3f774d4c77877108de371c78efd108389510d9
Ievgenij_Pavlenko <evgenij.pavlenko@gmail.com>
2020-11-16 19:13:28 +0100

    correct...

---
+++ js-oop/game.js
@@ -0,0 +1,21 @@
+function GameObject(imageUrl) {
+    this.x = 0;
+    this.y = 0;
+    this.image = new Image(this.x, this.y);
+    if (imageUrl) {
+        this.image.src = imageUrl;
+    }
+    this.ready = false;
+}
+//Uncaught TypeError: Cannot set property 'onload' of undefined  (game.js:11 )
+GameObject.prototype.image.onload = function () {
+    this.ready = true;
+}
+
+const image1 = new GameObject('http://pngimg.com/uploads/goblin/goblin_PNG32.png');
+const image2 = new GameObject();
+image1.image.onload(undefined);
+// image2.onload();
+
+console.log(image1);
+console.log(image2);
