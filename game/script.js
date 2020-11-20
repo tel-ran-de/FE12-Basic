374371a5b5494d40e05f230a39e82574e81052b0
Ievgenij_Pavlenko <evgenij.pavlenko@gmail.com>
2020-11-20 17:30:48 +0100

    HW 2020-11-16 Hero Game #99

--- game/script.js
+++ game/script.js
@@ -1,6 +1,7 @@
 function GameObject(imageUrl) {
     this.x = 0;
     this.y = 0;
+    this.speed = 1;
     this.ready = false;
     this.image = new Image();
     this.image.onload = () => this.ready = true;
@@ -9,15 +10,17 @@ function GameObject(imageUrl) {
     }
 }
 
-GameObject.prototype.render = function(ctx) {
+GameObject.prototype.render = function (ctx) {
     if (this.ready) {
         ctx.drawImage(this.image, this.x, this.y);
     }
 }
-
+// const imageHero = new Image();
+// imageHero.src = 'images/hero.png';
 const background = new GameObject('images/background.png');
 const hero = new GameObject('images/hero.png');
 const monster = new GameObject('images/monster.png');
+console.log(monster.width);
 
 const canvas = document.createElement('canvas');
 canvas.width = 512;
@@ -32,16 +35,38 @@ monster.y = monster.image.height + (Math.random() * (canvas.height - monster.ima
 
 const ctx = canvas.getContext('2d');
 
+hero.moveRight = function () {
+    this.x += 1 * this.speed*coef;
+}
+
+let coef;
+
 window.addEventListener('keydown', (event) => {
     console.log(event);
+    if (event.code === "ControlLeft") {
+        coef = 4;
+    } else if (event.code === "ControlRight") {
+        coef = 0.25;
+    } else {
+        coef = 1;
+    }
+    hero.moveUp = function () {
+        this.y -= 1 * this.speed*coef;
+    }
     if (event.key === "ArrowUp") {
         hero.moveUp();
+    }
+    hero.moveDown = function () {
+        this.y += 1 * this.speed*coef;
     }
     if (event.key === "ArrowDown") {
         hero.moveDown();
     }
     if (event.key === "ArrowRight") {
         hero.moveRight();
+    }
+    hero.moveLeft = function () {
+        this.x -= 1 * this.speed*coef;
     }
     if (event.key === "ArrowLeft") {
         hero.moveLeft();
@@ -52,7 +77,7 @@ window.addEventListener('keyup', (event) => {
     console.log(event);
 });
 
-const gameCycle = function() {
+const gameCycle = function () {
     background.render(ctx);
     hero.render(ctx);
     monster.render(ctx);
