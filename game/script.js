423c0572b71a2a7d770cf68577b4523bf0dcc922
Ievgenij_Pavlenko <evgenij.pavlenko@gmail.com>
2020-11-30 20:10:06 +0100

    add second monster

--- game/script.js
+++ game/script.js
@@ -6,6 +6,7 @@ document.body.appendChild(canvas);
 const background = new GameObject('images/background.png');
 const hero = createHero('images/hero.png', canvas);
 const monster = createMonster('images/monster.png', canvas);
+const monster2 = createMonster('images/monster.png', canvas);
 const score = createScore();
 
 const distanceBetweenTwoPoints = (x1, x2, y1, y2) => Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
@@ -37,11 +38,19 @@ const gameCycle = function() {
         monster.reset();
         console.log("Hero caught monster!!!")
     }
+    if (distanceBetweenTwoPoints(hero.x, monster2.x, hero.y, monster2.y) < hero.image.width / 2 + monster2.image.width / 2) {
+        score.wins++;
+        hero.reset();
+        monster2.reset();
+        console.log("Hero caught monster!!!")
+    }
     monster.update(delta);
+    monster2.update(delta);
 
     background.render(ctx);
     hero.render(ctx);
     monster.render(ctx);
+    monster2.render(ctx);
     score.render(ctx);
 
     before = now;
