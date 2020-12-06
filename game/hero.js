bb368d917032015a40dd2200982396b1ba51d887
Ievgenij_Pavlenko <evgenij.pavlenko@gmail.com>
2020-12-06 20:00:45 +0100

    Refactor Monster and Score using JS classes

--- game/hero.js
+++ game/hero.js
@@ -1,18 +1,18 @@
 class Hero extends MovableGameObject {
 
-    #canvasWidth;
-    #canvasHeight;
+    canvasWidth;
+    canvasHeight;
 
     constructor(imageUrl, canvas) {
         super(imageUrl);
-        this.#canvasWidth = canvas.width;
-        this.#canvasHeight = canvas.height;
+        this.canvasWidth = canvas.width;
+        this.canvasHeight = canvas.height;
         this.reset()
     }
 
     reset() {
-        this.x = this.#canvasWidth / 2;
-        this.y = this.#canvasHeight / 2;
+        this.x = this.canvasWidth / 2;
+        this.y = this.canvasHeight / 2;
         this.speed = 0.1;
     }
 
--- game/score.js
+++ game/score.js
@@ -1,27 +1,31 @@
-function createScore(time) {
-    const score = new GameObject();
-    const timeOfGame = time;
-    score.x = 32;
-    score.y = 32;
-    score.wins = 0;
-    score.timeToEnd = timeOfGame;
-    score.isGameOver = false;
+class Score{
+    #timeOfGame
+    #timeToEnd
 
-    score.update = function (delta) {
-        if (this.timeToEnd <= 0) {
+    constructor(time) {
+        this.#timeOfGame = time;
+        this.x = 32;
+        this.y = 32;
+        this.wins = 0;
+        this.#timeToEnd = this.#timeOfGame;
+        this.isGameOver = false;
+    }
+
+    update(delta) {
+        if (this.#timeToEnd <= 0) {
             this.isGameOver = true;
-            this.timeToEnd = 0;
+            this.#timeToEnd = 0;
         } else {
-            this.timeToEnd -= delta;
+            this.#timeToEnd -= delta;
         }
 
         if (keysPressed["Space"]){
             this.isGameOver = false;
-            this.timeToEnd = timeOfGame;
+            this.#timeToEnd = this.#timeOfGame;
         }
     }
 
-    score.render = function (ctx) {
+    render(ctx) {
         ctx.fillStyle = "rgb(250, 250, 250)";
         ctx.font = "24px Helvetica";
         ctx.textAlign = "left";
@@ -32,8 +36,41 @@ function createScore(time) {
         }
         else {
             ctx.fillText("Monsters caught:" + this.wins, this.x, this.y);
-            ctx.fillText("Seconds left: " + Math.floor(this.timeToEnd / 1000), this.x + 250, this.y);
+            ctx.fillText("Seconds left: " + Math.floor(this.#timeToEnd / 1000), this.x + 250, this.y);
         }
     }
-    return score;
+
 }
+
+// function createScore(time) {
+
+
+    // score.update = function (delta) {
+    //     if (this.timeToEnd <= 0) {
+    //         this.isGameOver = true;
+    //         this.timeToEnd = 0;
+    //     } else {
+    //         this.timeToEnd -= delta;
+    //     }
+    //
+    //     if (keysPressed["Space"]){
+    //         this.isGameOver = false;
+    //         this.timeToEnd = timeOfGame;
+    //     }
+    // }
+    //
+    // score.render = function (ctx) {
+    //     ctx.fillStyle = "rgb(250, 250, 250)";
+    //     ctx.font = "24px Helvetica";
+    //     ctx.textAlign = "left";
+    //     ctx.textBaseline = "top";
+    //     if (this.isGameOver) {
+    //         ctx.fillText("The game is over! Your score is: " + this.wins, this.x + 50, this.y + 150);
+    //         ctx.fillText("If you want to play again, press Space", this.x + 30, this.y + 200);
+    //     }
+    //     else {
+    //         ctx.fillText("Monsters caught:" + this.wins, this.x, this.y);
+    //         ctx.fillText("Seconds left: " + Math.floor(this.timeToEnd / 1000), this.x + 250, this.y);
+    //     }
+    // }
+    // return score;
