ce1125a05989b76442822781ac42b3d32be16654
isalexlog <is.alex.loginov@gmail.com>
2020-12-14 19:49:05 +0100

    HW 2020-12-11 - paragraphs changing colors on click #139 - solution

--- closures_paragraphs/script.js
+++ closures_paragraphs/script.js
@@ -1,2 +1,16 @@
 const colors = ['red', 'blue', 'green'];
 
+const createEventHandler = () => {
+    let i = 0;
+    return function() {
+        this.style.backgroundColor = colors[i % colors.length];
+        i++;
+    }
+}
+
+const paragraphs = document.querySelectorAll('p');
+
+paragraphs.forEach(element => {
+        element.onclick = createEventHandler();
+    }
+);
