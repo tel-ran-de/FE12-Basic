52bc6e7c3e61c3cb66821de24b86b8648e6b61d7
Ievgenij_Pavlenko <evgenij.pavlenko@gmail.com>
2020-12-14 14:09:57 +0100

    <b>backgroundColor</b> correct

--- closures/paragraph-scrypt.js
+++ closures/paragraph-scrypt.js
@@ -1,14 +1,16 @@
 const color = ['red', 'blue', 'green'];
 
-for (let i = 1; i < 4; i++) {
+for (let i = 1; i < color.length; i++) {
     let $paragraph = document.getElementById('p' + i);
     $paragraph.onclick = function (i) {
         let n = 0;
         return function (event) {
-            if (n > 2) {
-                this.style.color = null;
-                n = 0;
-            } else this.style.color = color[n];
+            if (n < color.length) {
+                this.style.backgroundColor = color[n];
+            } else {
+                this.style.backgroundColor = null;
+                n = -1;
+            }
             n++;
         };
     }(i);
