7dc0fbf70c13409e27ad10feadb0313ca8d02b43
isalexlog <is.alex.loginov@gmail.com>
2020-12-14 21:42:25 +0100

    CW - promise exercise #146 - to be finished at home (see comments)

--- promises/script.js
+++ promises/script.js
@@ -1,10 +1,38 @@
+const $p = document.getElementsByTagName('p')[0];
 
-// Simple promise example
-const somePromise = new Promise((resolve) => {
-    setTimeout(() => resolve("Hello from promise"), 5000)
+let i = 0;
+
+const handlerId = setInterval(() => {
+    i++;
+    $p.innerText = i;
+}, 1000); // replace with counter.start(interval)
+
+let result = false; // Generate randomly: 50% - true, 50% - false
+let delay = 5000; // Generate randomly: from 1000 to 5000 with 1000 step
+
+// counter object with two methods: counter.start, counter.stop
+
+const promise = new Promise((resolve, reject) => {
+    if (result) {
+        setTimeout(() => {
+            resolve('Hello');
+        }, delay);
+    } else {
+        setTimeout(() => {
+            reject('Error')
+        }, delay)
+    }
 });
 
-somePromise
-    .then((str) => console.log(str))
-
-console.log("Hello from usual js script");
+promise
+    .then(response => {
+        clearInterval(handlerId); // replace with counter.stop()
+        $p.innerText = response;
+        // change text color to green
+    })
+    .catch(reason => {
+        clearInterval(handlerId); // replace with counter.stop()
+        $p.innerText = reason;
+        // change text color to red
+    })
+    .finally(() => console.log('Request is finished'));
