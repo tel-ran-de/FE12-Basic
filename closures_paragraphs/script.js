dc90d83fd7aab1594d10efee9d952cc637fecde3
filolozka <hhshkola@gmail.com>
2020-12-14 13:29:22 +0100

    hw11-12 with changing backgroundColor

--- closures_paragraphs/script.js
+++ closures_paragraphs/script.js
@@ -1,2 +1,14 @@
 const colors = ['red', 'blue', 'green'];
 
+
+const paragraphs = document.getElementsByTagName('p');
+for (var j = 0; j < paragraphs.length; j++) {
+    let i = 0;
+    paragraphs[j].onclick = function (i) {
+        return function (event) {
+            i = (i + 1) % colors.length;
+            this.style.backgroundColor = colors[i];
+        }
+    }(i);
+}
+
